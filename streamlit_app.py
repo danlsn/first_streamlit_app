216bb40ef331d91acd3f026bf6199ca7a9bdb5bc
Dan Lawson <52593003+danlsn@users.noreply.github.com>
2023-06-01 14:33:44 +1000

    Update streamlit_app.py Add Header Add Menu Items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+# Add H2 title with a linebreak before
+streamlit.header('Breakfast Menu')
+
+# Add Menu Items
+streamlit.write('Omega 3 & Blueberry Oatmeal')
+streamlit.write('Kale, Spinach & Rocket Smoothie')
+streamlit.write('Hard-Boiled Free-Range Egg')
