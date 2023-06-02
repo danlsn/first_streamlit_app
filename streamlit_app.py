556b605506c2359526c1412a43a8706a620c1933
Dan Lawson <52593003+danlsn@users.noreply.github.com>
2023-06-02 11:38:50 +1000

    Update streamlit_app.py Add logic to fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,21 +31,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-fruit_search = "kiwi"
-fruityvice_response = requests.get(
-    f"https://www.fruityvice.com/api/fruit/{fruit_search}"
-)
+
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+    fruit_choice = streamlit.text_input(
+        "What fruit would you like information about?"
+    )
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get(f"https://www.fruityvice.com/api/fruit/{fruit_choice}").json()
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-fruit_choice = streamlit.text_input(
-    "What fruit would you like information about?", "Kiwi"
-)
-streamlit.write("The user entered ", fruit_choice)
-
-# Load JSON into DataFrame using pd.json_normalize
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# Display the table on the page.
-streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
