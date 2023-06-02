c4f86ae3f85124e5ce11ec039f2c9fac8b246cd7
Dan Lawson <52593003+danlsn@users.noreply.github.com>
2023-06-02 11:15:26 +1000

    Update streamlit_app.py Create snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,53 @@ import pandas as pd
 import requests
 import snowflake.connector
 
-streamlit.title('My Parents New Healthy Diner')
+
+streamlit.title("My Parents New Healthy Diner")
 
 # Add H2 title with a linebreak before
-streamlit.header('Breakfast Menu')
+streamlit.header("Breakfast Menu")
 
 # Add Menu Items
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+my_fruit_list = pd.read_csv(
+    "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+)
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
 
 # Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect(
+    "Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"]
+)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 fruit_search = "kiwi"
-fruityvice_response = requests.get(f"https://www.fruityvice.com/api/fruit/{fruit_search}")
+fruityvice_response = requests.get(
+    f"https://www.fruityvice.com/api/fruit/{fruit_search}"
+)
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input(
+    "What fruit would you like information about?", "Kiwi"
+)
+streamlit.write("The user entered ", fruit_choice)
 
 # Load JSON into DataFrame using pd.json_normalize
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # Display the table on the page.
 streamlit.dataframe(fruityvice_normalized)
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
